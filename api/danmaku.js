c344205771afcf8adca73057db0d200972f0f766
niyuancheng <nyc20010820@126.com>
2023-02-16 21:45:08 +0800

    tiny change

--- api/danmaku.js
+++ api/danmaku.js
@@ -1,19 +1,16 @@
 //TODO 专门为部署到vercel服务器上写的接口
-// var { XML2DanmakuData } = require("../parseDanmaku");
+var { XML2DanmakuData } = require("../parseDanmaku");
 
 module.exports = (req, res) => {
-//   let time = req.query.time;
+  let time = req.query.time;
 
-//   let danmaku = XML2DanmakuData("../assets/bilibili-danmaku.xml");
+  let danmaku = XML2DanmakuData("../assets/bilibili-danmaku.xml");
 
-//   let data = [];
-//   for (let index in danmaku) {
-//     if (Math.abs(danmaku[index].timestamp - time) < 0.5) {
-//       data.push(danmaku[index]);
-//     }
-//   }
-  res.json(JSON.stringify({
-    name:"json",
-    age:"10"
-  }));
+  let data = [];
+  for (let index in danmaku) {
+    if (Math.abs(danmaku[index].timestamp - time) < 0.5) {
+      data.push(danmaku[index]);
+    }
+  }
+  res.send(JSON.stringify(data));
 };
