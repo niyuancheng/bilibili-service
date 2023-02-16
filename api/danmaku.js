4275acd22e49b27416391adeea72c7cc4d497d68
niyuancheng <nyc20010820@126.com>
2023-02-16 22:15:11 +0800

    tiny change

--- api/danmaku.js
+++ api/danmaku.js
@@ -6,15 +6,13 @@ var {str} = require("../danmakuStr")
 module.exports = (req, res) => {
   let time = req.query.time;
 
-  let res = parseBilibiliXML(str)
+  let danamku = parseBilibiliXML(str)
 
   function cmp(obj1,obj2) {
       return obj1.time - obj2.time
   }
 
   res.sort(cmp);
-
-  let danmaku = res;
 
   let data = [];
   for (let index in danmaku) {
