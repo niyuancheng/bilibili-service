4fad4279d5864969a15ca8c63f8298fcfdf5404e
niyuancheng <nyc20010820@126.com>
2023-02-16 21:01:46 +0800

    modified:   api/danmaku.js

--- api/danmaku.js
+++ api/danmaku.js
@@ -2,15 +2,18 @@
 var { XML2DanmakuData } = require("./parseDanmaku");
 
 module.exports = (req, res) => {
-  let time = req.query.time;
+//   let time = req.query.time;
 
-  let danmaku = XML2DanmakuData("../assets/bilibili-danmaku.xml");
+//   let danmaku = XML2DanmakuData("../assets/bilibili-danmaku.xml");
 
-  let data = [];
-  for (let index in danmaku) {
-    if (Math.abs(danmaku[index].timestamp - time) < 0.5) {
-      data.push(danmaku[index]);
-    }
-  }
-  res.json(data);
+//   let data = [];
+//   for (let index in danmaku) {
+//     if (Math.abs(danmaku[index].timestamp - time) < 0.5) {
+//       data.push(danmaku[index]);
+//     }
+//   }
+  res.json({
+    name:"json",
+    age:"10"
+  });
 };
